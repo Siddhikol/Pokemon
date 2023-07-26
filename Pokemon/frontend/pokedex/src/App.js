c10fc895a86202bffb0168bf98b85594a0d6984b
Harsh <harshchuri7@gmail.com>
2023-07-26 05:10:23 -1200

    initial commit

---
+++ Pokemon/frontend/pokedex/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       Pokedex
+      </header>
+      <div>
+        Pokedex App
+      </div>
+    </div>
+  );
+}
+
+export default App;
